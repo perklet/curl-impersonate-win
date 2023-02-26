64381a72048bbdba7a329b6bd5f54cbd655b4709
Yifei Kong <kong@yifei.me>
2023-02-26 21:40:19 +0800

    Add curl-impersonate as a submodule

--- copy_and_patch.bat
+++ copy_and_patch.bat
@@ -1,7 +1,23 @@
-xcopy .\boringssl_original\* .\boringssl\ /r /d /i /s /y /exclude:.\patch\excludecopy.txt
-xcopy .\curl_original\* .\curl\ /r /d /i /s /y /exclude:.\patch\excludecopy.txt
+xcopy .\boringssl_original\* .\boringssl\ /r /d /i /s /y /exclude:.\excludecopy.txt
+xcopy .\curl_original\* .\curl\ /r /d /i /s /y /exclude:.\excludecopy.txt
 
-start patch_boringssl.bat
-start patch_curl.bat
+set patch=c:\msys64\usr\bin\patch.exe
+set sed=c:\msys64\usr\bin\sed.exe
+
+cd %~dp0\boringssl
+%patch% -p1 < %~dp0\curl-impersonate\chrome\patches\boringssl-old-ciphers.patch
+%sed% -i 's/-ggdb//g' CMakeLists.txt
+%sed% -i 's/-Werror//g' CMakeLists.txt
+
+cd %~dp0\curl
+%patch% -p1 < %~dp0\curl-impersonate\chrome\patches\curl-impersonate.patch
+%sed% -i 's/-shared/-s -static -shared/g' lib\Makefile.m32
+%sed% -i 's/-static/-s -static/g' src\Makefile.m32
+
+%sed% -i 's/-DUSE_NGHTTP2/-DUSE_NGHTTP2 -DNGHTTP2_STATICLIB/g' lib\Makefile.m32
+%sed% -i 's/-DUSE_NGHTTP2/-DUSE_NGHTTP2 -DNGHTTP2_STATICLIB/g' src\Makefile.m32
+
+%sed% -i 's/-lidn2/-lidn2 -lunistring -liconv/g' lib\Makefile.m32
+%sed% -i 's/-lidn2/-lidn2 -lunistring -liconv/g' src\Makefile.m32
 
 pause
